6d1cd9a71eb5c0c30d18dd9f3e098419ea1022c8
technifutur <technifutur@DESKTOP-EHN422D>
2023-05-08 11:55:11 +0200

    Add database project

---
+++ Database/InitialData/Script.PostDeployment.sql
@@ -0,0 +1,12 @@
+﻿
+DELETE FROM [UserApp];
+
+SET IDENTITY_INSERT [UserApp] ON;  
+
+INSERT [UserApp] (User_Id, Firstname, Lastname, Birthdate, Email, Password_Hash)
+ VALUES (1, 'Della', 'Duck', '1988-02-06', 'della.duck@gmail.com', '$argon2i$v=19$m=16,t=2,p=1$RGV2T3BzMjAyMw$IOUuuYue1DMMuUA9RdeBDg'),
+		(2, 'Zaza', 'Vanderquack', '2001-11-11', 'zaza@outlook.com', '$argon2i$v=19$m=16,t=2,p=1$RGV2T3BzMjAyMg$FF65e+TqpXFttc/BenlMWg');
+
+SET IDENTITY_INSERT [UserApp] OFF;  
+
+
---
+++ Database/Tables/UserApp.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserApp]
+(
+	[User_Id] INT NOT NULL IDENTITY,
+	[Firstname] NVARCHAR(50) NOT NULL,
+	[Lastname] NVARCHAR(50) NOT NULL,
+	[Email] NVARCHAR(200) NOT NULL,
+	[Birthdate] DATE NOT NULL,
+	[Password_Hash] CHAR(64) NOT NULL, 
+
+	CONSTRAINT PK_UserApp PRIMARY KEY ([User_Id]),
+	CONSTRAINT UK_UserApp__Email UNIQUE([Email])
+);
