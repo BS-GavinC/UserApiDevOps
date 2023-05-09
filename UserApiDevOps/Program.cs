e0c2fbb0cf921df7f65b8692246a021f7228cc1c
technifutur <technifutur@DESKTOP-EHN422D>
2023-05-09 10:28:27 +0200

    Add doc Swagger

--- UserApiDevOps/Program.cs
+++ UserApiDevOps/Program.cs
@@ -6,6 +6,7 @@ using BLL.Services;
 using DAL.Interfaces;
 using DAL.Repositories;
 using Microsoft.Data.SqlClient;
+using Microsoft.OpenApi.Models;
 using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // - DAL
-builder.Services.AddTransient<IDbConnection, SqlConnection>((service) => {
+builder.Services.AddTransient<IDbConnection, SqlConnection>((service) =>
+{
     string connectionString = builder.Configuration.GetConnectionString("Default");
     return new SqlConnection(connectionString);
 });
@@ -24,11 +26,52 @@ builder.Services.AddScoped<IUserService, UserService>();
 // - API Helper
 builder.Services.AddSingleton<JwtHelper>();
 
-
+// Ajout des controllers de l'API
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// La doc Swagger
+// - Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Ajout de la doc pour swagger
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "UserApi DevOps",
+        Description = "Exemple d'API pour les DevOps de Techni",
+    });
+
+    // Ajout de l'authentification dans swagger
+    // - Bouton pour se connecter sur l'interface
+    options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Entrer votre JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    // - Lock sur les routes (Toutes les routes...)
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
